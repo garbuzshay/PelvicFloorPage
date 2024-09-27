e7945e56bdfe69fc4d1efb327c403847850668ba
garbuzshay <shay.garbuz@e.braude.ac.il>
2024-09-27 15:19:53 +0300

    fix sytles for sideBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'; // Import useRouter from Next.js
 import { auth } from '../lib/firebase';
@@ -48,21 +47,33 @@ export default function Navbar({ setSelectedSection }: NavbarProps) {
   };
 
   return (
-    <header dir="rtl" className={`${isOpen ? 'bg-transparent' : 'bg-gradient-to-r from-pink-300 to-purple-100'} text-black`}>
+    <header dir="rtl" className={`${isOpen ? 'bg-transparent' : 'bg-gradient-to-r from-pink-300 to-purple-100 dark:from-gray-800 dark:to-gray-900'} text-black dark:text-gray-300`}>
       <nav className="container mx-auto flex items-center justify-center">
         {/* Links Section */}
         <div className={`flex flex-col md:flex-row items-center ${isOpen ? 'block' : 'hidden'} md:block`}>
           <div className="flex flex-wrap justify-center space-x-6 p-4">
-            <button onClick={() => setSelectedSection('home')} className="text-xl px-4 font-bold hover:text-purple-700 transition duration-300">עמוד הבית</button>
-            <button onClick={() => setSelectedSection('treatments')} className="text-xl px-4 font-bold hover:text-purple-700 transition duration-300">טיפולים</button>
-            <button onClick={() => setSelectedSection('questions')} className="text-xl px-4 font-bold hover:text-purple-700 transition duration-300">שאלות ותשובות</button>
-            <button onClick={() => setSelectedSection('contact')} className="text-xl px-4 font-bold hover:text-purple-700 transition duration-300">יצירת קשר</button>
+            <button onClick={() => setSelectedSection('home')} className="text-xl px-4 font-bold hover:text-purple-700 dark:hover:text-purple-300 transition duration-300">
+              עמוד הבית
+            </button>
+            <button onClick={() => setSelectedSection('treatments')} className="text-xl px-4 font-bold hover:text-purple-700 dark:hover:text-purple-300 transition duration-300">
+              טיפולים
+            </button>
+            <button onClick={() => setSelectedSection('questions')} className="text-xl px-4 font-bold hover:text-purple-700 dark:hover:text-purple-300 transition duration-300">
+              שאלות ותשובות
+            </button>
+            <button onClick={() => setSelectedSection('contact')} className="text-xl px-4 font-bold hover:text-purple-700 dark:hover:text-purple-300 transition duration-300">
+              יצירת קשר
+            </button>
             {!isAuthenticated ? (
-              <button onClick={handleLogin} className="text-xl px-4 font-bold hover:text-purple-700 transition duration-300">Login</button>
+              <button onClick={handleLogin} className="text-xl px-4 font-bold hover:text-purple-700 dark:hover:text-purple-300 transition duration-300">
+                Login
+              </button>
             ) : (
               <>
-                <button onClick={() => setSelectedSection('edit')} className="text-xl font-bold hover:text-purple-700 transition duration-300">עריכת תוכן</button>
-                <button onClick={handleLogout} className="bg-red-600 hover:bg-red-500 text-white py-1 px-3 rounded transition duration-300">
+                <button onClick={() => setSelectedSection('edit')} className="text-xl font-bold hover:text-purple-700 dark:hover:text-purple-300 transition duration-300">
+                  עריכת תוכן
+                </button>
+                <button onClick={handleLogout} className="bg-red-600 hover:bg-red-500 dark:bg-red-700 dark:hover:bg-red-600 text-white py-1 px-3 rounded transition duration-300">
                   Logout
                 </button>
               </>
@@ -73,7 +84,7 @@ export default function Navbar({ setSelectedSection }: NavbarProps) {
         {/* Hamburger Menu */}
         <button 
           onClick={toggleSidebar} 
-          className="md:hidden focus:outline-none fixed right-4 top-4 z-50"
+          className="md:hidden focus:outline-none fixed right-4 top-4 z-50 text-gray-800 dark:text-gray-300"
         >
           <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
@@ -84,22 +95,36 @@ export default function Navbar({ setSelectedSection }: NavbarProps) {
       {/* Sidebar for mobile */}
       {isOpen && (
         <div 
-          className="fixed inset-0 bg-white bg-opacity-90 z-50 p-4 md:hidden transition-all duration-300"
+          className="fixed inset-0 bg-white dark:bg-gray-800 bg-opacity-90 dark:bg-opacity-90 z-50 p-4 md:hidden transition-all duration-300"
           onTouchStart={handleTouchStart} // Detect touch start
           onTouchMove={handleTouchMove}   // Detect swipe
         >
           <div className="flex flex-col space-y-4">
-            <button onClick={toggleSidebar} className="self-end text-black">Close</button>
-            <button onClick={() => setSelectedSection('home')} className="text-lg font-bold hover:text-purple-700">עמוד הבית</button>
-            <button onClick={() => setSelectedSection('treatments')} className="text-lg font-bold hover:text-purple-700">טיפולים</button>
-            <button onClick={() => setSelectedSection('questions')} className="text-lg font-bold hover:text-purple-700">שאלות ותשובות</button>
-            <button onClick={() => setSelectedSection('contact')} className="text-lg font-bold hover:text-purple-700">יצירת קשר</button>
+            <button onClick={toggleSidebar} className="self-end text-black dark:text-gray-300">
+              Close
+            </button>
+            <button onClick={() => setSelectedSection('home')} className="text-lg font-bold hover:text-purple-700 dark:hover:text-purple-300">
+              עמוד הבית
+            </button>
+            <button onClick={() => setSelectedSection('treatments')} className="text-lg font-bold hover:text-purple-700 dark:hover:text-purple-300">
+              טיפולים
+            </button>
+            <button onClick={() => setSelectedSection('questions')} className="text-lg font-bold hover:text-purple-700 dark:hover:text-purple-300">
+              שאלות ותשובות
+            </button>
+            <button onClick={() => setSelectedSection('contact')} className="text-lg font-bold hover:text-purple-700 dark:hover:text-purple-300">
+              יצירת קשר
+            </button>
             {!isAuthenticated ? (
-              <button onClick={handleLogin} className="text-lg font-bold hover:text-purple-700">Login</button>
+              <button onClick={handleLogin} className="text-lg font-bold hover:text-purple-700 dark:hover:text-purple-300">
+                Login
+              </button>
             ) : (
               <>
-                <button onClick={() => setSelectedSection('edit')} className="text-lg font-bold hover:text-purple-700">עריכת תוכן</button>
-                <button onClick={handleLogout} className="bg-red-600 hover:bg-red-500 text-white py-1 px-3 rounded">
+                <button onClick={() => setSelectedSection('edit')} className="text-lg font-bold hover:text-purple-700 dark:hover:text-purple-300">
+                  עריכת תוכן
+                </button>
+                <button onClick={handleLogout} className="bg-red-600 hover:bg-red-500 dark:bg-red-700 dark:hover:bg-red-600 text-white py-1 px-3 rounded">
                   Logout
                 </button>
               </>
